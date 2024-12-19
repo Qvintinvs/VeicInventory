e2b5528d7b7a08ba7f14ddf88496d315fd0831ae
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-19 11:54:18 -0300

    added vehicle db model

---
+++ web_server/models/vehicle.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+db = SQLAlchemy(model_class=DeclarativeBase)
+
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, nullable=False)
+    fuel = db.Column(db.String(50), nullable=False)
+    subcategory = db.Column(db.String(100), nullable=False)
