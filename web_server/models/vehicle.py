76e6ed782d70f82a9adaeb2f8a5c61cffeb081b9
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-20 13:58:01 -0300

    defined database insertion method

--- web_server/models/vehicle.py
+++ web_server/models/vehicle.py
@@ -1,10 +1,20 @@
+from typing import NamedTuple
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
 db = SQLAlchemy(model_class=DeclarativeBase)
 
 
-class Vehicle(db.Model):
+class VehicularData(NamedTuple):
+    year: tuple[int]
+    fuel: tuple[str]
+    subcategory: tuple[str]
+
+
+class VasquesVehicleModel(db.Model):
+    __tablename__ = "vehicles"
+
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer, nullable=False)
     fuel = db.Column(db.String(50), nullable=False)
@@ -14,3 +24,16 @@ class Vehicle(db.Model):
         self.year = year
         self.fuel = fuel
         self.subcategory = subcategory
+
+    @classmethod
+    def insert_multiple_vehicles_from(cls, vehicles_data: VehicularData):
+        years, fuels, subcategories = vehicles_data
+
+        vehicles = (
+            cls(year, fuel, subcategory)
+            for year, fuel, subcategory in zip(years, fuels, subcategories)
+        )
+
+        db.session.add_all(vehicles)
+
+        db.session.commit()
