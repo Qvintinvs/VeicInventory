9b70987cf3c3cdc5cde91d8d3b4ebf54d5126201
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-20 22:04:22 -0300

    added the database setup in the app

--- web_server/app_container.py
+++ web_server/app_container.py
@@ -1,10 +1,14 @@
 from dependency_injector import containers, providers
+from flask_sqlalchemy import SQLAlchemy
+from models import vehicle
 from services import namelist_creator
 from views import index_view
 
 
 class InventoryAppContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
+
+    database = providers.Singleton(SQLAlchemy, model_class=vehicle.Base)
 
     main_page = providers.Singleton(
         index_view.IndexView,
