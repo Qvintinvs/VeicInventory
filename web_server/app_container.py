adc59303b1ceefd0421a746c059c3f218bbfca46
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-21 11:46:36 -0300

    adding the inventory in the DI container

--- web_server/app_container.py
+++ web_server/app_container.py
@@ -1,6 +1,6 @@
 from dependency_injector import containers, providers
 from flask_sqlalchemy import SQLAlchemy
-from models import vehicle
+from models import vasques_vehicle_model, vehicles_database
 from services import namelist_creator
 from views import index_view
 
@@ -8,9 +8,14 @@ from views import index_view
 class InventoryAppContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
 
-    database = providers.Singleton(SQLAlchemy, model_class=vehicle.Base)
+    database = providers.Singleton(SQLAlchemy, model_class=vasques_vehicle_model.Base)
+
+    vehicular_inventory = providers.Singleton(
+        vehicles_database.VehiclesDatabase, db=database
+    )
 
     main_page = providers.Singleton(
         index_view.IndexView,
+        vehicular_inventory=vehicular_inventory,
         namelist_creator=namelist_creator.NamelistContentCreator("emission_vehicles"),
     )
