a030baae369fa996b591645cce388d2757037eca
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-22 16:52:51 -0300

    added db initialization

--- web_server/models/vehicles_database.py
+++ web_server/models/vehicles_database.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models.vasques_vehicle_model import VasquesVehicleModel, VehicularData
 
@@ -5,6 +6,12 @@ from models.vasques_vehicle_model import VasquesVehicleModel, VehicularData
 class VehiclesDatabase:
     def __init__(self, db: SQLAlchemy):
         self.__db = db
+
+    def initialize_database_in(self, app: Flask):
+        self.__db.init_app(app)
+
+        with app.app_context():
+            self.__db.create_all()
 
     def insert_vehicles_of(self, veic: VehicularData):
         vehicles = VasquesVehicleModel.create_vehicles_from(veic)
