6221ebe6838f39c7e01db8be8539cd2fc294b5bd
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-26 22:45:17 -0300

    added properties for the ORM class

--- web_server/models/vasques_vehicle_model.py
+++ web_server/models/vasques_vehicle_model.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 from sqlalchemy import CHAR, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
@@ -8,11 +10,23 @@ class VasquesVehicleModel(Base):
     __tablename__ = "vehicle"
 
     id = Column(Integer, primary_key=True)
-    year = Column(Integer, nullable=False)
-    fuel = Column(String(50), nullable=False)
-    subcategory = Column(CHAR, nullable=False)
+    _year = Column(Integer, nullable=False)
+    _fuel = Column(String(50), nullable=False)
+    _subcategory = Column(CHAR, nullable=False)
 
     def __init__(self, year: int, fuel: str, subcategory: str):
-        self.year = year
-        self.fuel = fuel
-        self.subcategory = subcategory
+        self._year = year
+        self._fuel = fuel
+        self._subcategory = subcategory
+
+    @property
+    def year(self):
+        return cast(int, self._year)
+
+    @property
+    def fuel(self):
+        return cast(str, self._fuel)
+
+    @property
+    def subcategory(self):
+        return cast(str, self._subcategory)
