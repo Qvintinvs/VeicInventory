2396ee534bc7a0239a303a87b7bd5958a1c605ab
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-01-22 14:45:04 -0300

    removed unused import

--- web_server/views/main_form_view.py
+++ web_server/views/main_form_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, redirect, render_template, url_for
 from services.vehicles_repository import VehiclesRepository
 from views.vasques_vehicle_form import VasquesVehicleForm
 from views.vehicle_interactions_form import VehicleInteractionsForm
