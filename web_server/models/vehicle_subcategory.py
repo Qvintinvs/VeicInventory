44d5d199bd7858fb7fbbad907ed83a3466a8f6cc
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-01-27 20:43:26 -0300

    removed unused to_dict conversion method

--- web_server/models/vehicle_subcategory.py
+++ web_server/models/vehicle_subcategory.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from types import MappingProxyType
 from typing import LiteralString
 
 
@@ -8,12 +7,3 @@ class VehicleSubcategory:
     name: LiteralString
     deterioration_factor: float
     category_consumption: float
-
-    def to_dict(self):
-        return MappingProxyType(
-            {
-                "subcategory_name": self.name,
-                "deterioration_factor": self.deterioration_factor,
-                "category_consumption": self.category_consumption,
-            }
-        )
