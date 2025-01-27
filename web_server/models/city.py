80ca48d925e7e68a722f050a1453b536bfbbcc4c
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-01-27 11:50:03 -0300

    added a constructor for the city model

--- web_server/models/city.py
+++ web_server/models/city.py
@@ -6,5 +6,9 @@ class City(Base):
     __tablename__ = "city"
 
     id = Column(Integer, primary_key=True)
-    city_name = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
     fuel_consumption = Column(Float, nullable=False)
+
+    def __init__(self, name: str, fuel_consumption: float):
+        self.name = name
+        self.fuel_consumption = fuel_consumption
