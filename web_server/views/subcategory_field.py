74bf77c4d8a21d78d7e783e8e9f4926b328d8257
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-01-28 15:20:07 -0300

    modularized form subcategory field into a specific class

---
+++ web_server/views/subcategory_field.py
@@ -0,0 +1,41 @@
+from models.cnh_subcategory import CNHSubcategory
+from wtforms import SelectField
+from wtforms.validators import AnyOf, DataRequired
+
+
+class SubcategoryField(SelectField):
+    def __init__(self, **kwargs):
+        label = "Subcategoria:"
+
+        choices = (
+            ("", "Selecione a subcategoria de CNH..."),
+            ("A", "A - Motos, motonetas e triciclos"),
+            ("B", "B - Carros de passeio, utilitário/SUV, minivan, picape"),
+            ("C", "C - Caminhões, caminhonetes, vans de carga"),
+            ("D", "D - Ônibus, microônibus, vans de passageiros"),
+            ("E", "E - Veículos pesados"),
+        )
+
+        validators = (
+            DataRequired(),
+            AnyOf(("A", "B", "C", "D", "E")),
+        )
+
+        super().__init__(label=label, choices=choices, validators=validators, **kwargs)
+
+    @property
+    def vehicle_subcategory(self):
+        form_value: str | None = self.data
+
+        matches_in_the_cnh_subcategories = (
+            subcategory.value
+            for subcategory in CNHSubcategory
+            if subcategory.name == form_value
+        )
+
+        subcategory_of_the_value = next(matches_in_the_cnh_subcategories, None)
+
+        if subcategory_of_the_value is None:
+            raise ValueError(f"Invalid value for CNHSubcategory: {form_value}")
+
+        return subcategory_of_the_value
--- web_server/views/vasques_vehicle_form.py
+++ web_server/views/vasques_vehicle_form.py
@@ -2,8 +2,8 @@ from typing import cast
 
 from flask_wtf import FlaskForm
 from models.city import City
-from models.cnh_subcategory import CNHSubcategory
 from models.vasques_vehicle_model import VasquesVehicleModel
+from views.subcategory_field import SubcategoryField
 from wtforms import FloatField, IntegerField, SelectField, SubmitField
 from wtforms.validators import AnyOf, DataRequired, NumberRange
 
@@ -31,21 +31,7 @@ class VasquesVehicleForm(FlaskForm):
         ),
     )
 
-    subcategory = SelectField(
-        "Subcategoria:",
-        choices=(
-            ("", "Selecione a subcategoria de CNH..."),
-            ("A", "A - Motos, motonetas e triciclos"),
-            ("B", "B - Carros de passeio, utilitário/SUV, minivan, picape"),
-            ("C", "C - Caminhões, caminhonetes, vans de carga"),
-            ("D", "D - Ônibus, microônibus, vans de passageiros"),
-            ("E", "E - Veículos pesados"),
-        ),
-        validators=(
-            DataRequired(),
-            AnyOf(("A", "B", "C", "D", "E")),
-        ),
-    )
+    subcategory = SubcategoryField()
 
     exhaust_emission_factor = FloatField(
         "Fator de Emissão por Exaustão:",
@@ -62,30 +48,13 @@ class VasquesVehicleForm(FlaskForm):
     submit = SubmitField("Salvar")
 
     @property
-    def vehicle_subcategory(self):
-        form_value: str | None = self.subcategory.data
-
-        matches_in_the_cnh_subcategories = (
-            subcategory.value
-            for subcategory in CNHSubcategory
-            if subcategory.name == form_value
-        )
-
-        subcategory_of_the_value = next(matches_in_the_cnh_subcategories, None)
-
-        if subcategory_of_the_value is None:
-            raise ValueError(f"Invalid value for CNHSubcategory: {form_value}")
-
-        return subcategory_of_the_value
-
-    @property
     def vehicle(self):
         example_city = City("Itajaí", 1.1)
 
         return VasquesVehicleModel(
             cast(int, self.year.data),
             cast(str, self.fuel.data),
-            self.vehicle_subcategory,
+            self.subcategory.vehicle_subcategory,
             cast(float, self.exhaust_emission_factor.data),
             cast(float, self.autonomy.data),
             example_city,
