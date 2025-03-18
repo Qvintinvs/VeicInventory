c924ce214641dc593aee2420d3defd48f72bebc8
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-03-18 06:58:54 -0300

    promoted constructor variables to class properties

--- web_server/views/vehicular_inventory_forms/subcategory_field.py
+++ web_server/views/vehicular_inventory_forms/subcategory_field.py
@@ -9,20 +9,23 @@ class SubcategoryField(SelectField):
         AnyOf(tuple(subcategory.name for subcategory in CNHSubcategory)),
     )
 
+    __label = "Subcategoria:"
+
+    __choices = (
+        ("", "Selecione a subcategoria de CNH..."),
+        ("A", "A - Motos, motonetas e triciclos"),
+        ("B", "B - Carros de passeio, utilitário/SUV, minivan, picape"),
+        ("C", "C - Caminhões, caminhonetes, vans de carga"),
+        ("D", "D - Ônibus, microônibus, vans de passageiros"),
+        ("E", "E - Veículos pesados"),
+    )
+
     def __init__(self, **kwargs):
-        label = "Subcategoria:"
-
-        choices = (
-            ("", "Selecione a subcategoria de CNH..."),
-            ("A", "A - Motos, motonetas e triciclos"),
-            ("B", "B - Carros de passeio, utilitário/SUV, minivan, picape"),
-            ("C", "C - Caminhões, caminhonetes, vans de carga"),
-            ("D", "D - Ônibus, microônibus, vans de passageiros"),
-            ("E", "E - Veículos pesados"),
-        )
-
         super().__init__(
-            label=label, choices=choices, validators=self.__validators, **kwargs
+            label=self.__label,
+            choices=self.__choices,
+            validators=self.__validators,
+            **kwargs,
         )
 
     @property
