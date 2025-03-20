2a07f4d6f136f64d118d01ce40e5901d97640b7b
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-03-20 11:28:07 -0300

    added a WRF rounds database to the DI container

--- web_server/app_container.py
+++ web_server/app_container.py
@@ -1,14 +1,25 @@
 from dependency_injector import containers, providers
 from flask_sqlalchemy import SQLAlchemy
 from models.base import Base
-from services import connection_settings, vehicles_repository, wrf_service
+from services import (
+    connection_settings,
+    vehicles_repository,
+    wrf_service,
+    wrf_rounds_repository,
+)
 
 
 class InventoryAppContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
 
+    sql_db = providers.Singleton(SQLAlchemy, model_class=Base)
+
     vehicular_inventory = providers.Singleton(
-        vehicles_repository.VehiclesRepository, sql_db=SQLAlchemy(model_class=Base)
+        vehicles_repository.VehiclesRepository, sql_db=sql_db
+    )
+
+    wrf_rounds_db = providers.Singleton(
+        wrf_rounds_repository.WRFRoundsRepository, sql_db=sql_db
     )
 
     connection_settings = providers.Singleton(
