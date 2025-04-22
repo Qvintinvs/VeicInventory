f2550b9d6cd37aceaa818ecc1ce4432df979ea3e
OQuintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-04-22 11:55:09 -0300

    Added NETCDF API

---
+++ web_server/routes/netcdf_api_routes.py
@@ -0,0 +1,24 @@
+from dependency_injector.wiring import Provide, inject
+from flask import Blueprint
+from inventory_app_container import InventoryAppContainer
+from services.netcdf_blob_repository import NETCDFBlobRepository
+from views.netcdf_api import NETCDFAPI
+
+
+@inject
+def create_netcdf_api_blueprint(
+    netcdf_blob_repository: NETCDFBlobRepository = Provide[
+        InventoryAppContainer.netcdf_blob_repository
+    ],
+):
+    wrf_round_api = NETCDFAPI(netcdf_blob_repository)
+
+    api_blueprint = Blueprint("netcdf_api", __name__, url_prefix="/netcdf_api")
+
+    api_blueprint.add_url_rule(
+        "/submit_netcdf_file_for_round",
+        view_func=wrf_round_api.complete_the_round,
+        methods=["POST"],
+    )
+
+    return api_blueprint
