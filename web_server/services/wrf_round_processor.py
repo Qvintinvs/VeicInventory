5101dd3501cc576e13648d13491a1583540701e9
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-05-21 15:08:13 -0300

    Changed the class responsability to manage the Redis queue

--- web_server/services/wrf_round_processor.py
+++ web_server/services/wrf_round_processor.py
@@ -1,24 +1,29 @@
-from multiprocessing import Process, Queue
+import json
 
+import redis
 from models.wrf_round import WRFRound
 
 
-class WRFRoundProcessor(Process):
-    def __init__(self, rounds_queue: Queue):
-        super().__init__()
-        self.__queue = rounds_queue
+# TODO: Rename queue
+# TODO: Add exceptions handling
+class WRFRoundProcessor:
+    def __init__(self, redis_client: redis.Redis):
+        self.__redis = redis_client
 
     def enqueue_round(self, wrf_round: WRFRound):
-        self.__queue.put(wrf_round)
+        round_json_dict = {
+            "id": wrf_round.id,
+            "namelist": wrf_round.namelist,
+            "output_file_path": wrf_round.output_file_path,
+        }
+
+        self.__redis.rpush("wrf-queue", json.dumps(round_json_dict))
 
     def send_next_round(self):
         try:
-            earliest_round: WRFRound | None = self.__queue.get(timeout=10)
+            return
         except Exception:
             return
-
-        if earliest_round:
-            pass
 
     def run(self):
         while True:
