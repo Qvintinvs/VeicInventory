df90485e6becf3f6029aa047ae66e91a58feb498
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-09-25 05:37:13 -0300

    added method to read the compressed minio file

--- web_server/repositories/wrfchemi_blobs.py
+++ web_server/repositories/wrfchemi_blobs.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+
+import zstandard as zstd
 from minio import Minio
 
 
@@ -20,6 +23,21 @@ class MinioRepository:
             obj.object_name
             for obj in self.__client.list_objects(self.__bucket_name, prefix=prefix)
         )
+
+    def read_file(self, prefix: str):
+        response = self.__client.get_object(
+            self.__bucket_name, f"wrfchemi/{prefix}.zst"
+        )
+
+        compressed_data = response.read()
+
+        response.close()
+        response.release_conn()
+
+        dctx = zstd.ZstdDecompressor()
+        decompressed_data = dctx.decompress(compressed_data)
+
+        return BytesIO(decompressed_data)
 
     def delete_file(self, object_name: str) -> None:
         """Remove um arquivo do MinIO"""
