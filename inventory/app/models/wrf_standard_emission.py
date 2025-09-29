b0530fa80fdd8596daecc05eac42f1a602c26d10
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-09-29 11:40:36 -0300

    moved the repositories layer to Django

--- inventory/app/models/wrf_standard_emission.py
+++ inventory/app/models/wrf_standard_emission.py
@@ -50,3 +50,14 @@ class WRFStandardEmission(models.Model):
     def add_round(self, wrf_round: "WRFRound"):
         wrf_round.vehicle = self
         wrf_round.save()
+
+
+class WRFStandardEmissionManager(models.Manager):
+    def list_emissions(self, limit=5) -> models.QuerySet[WRFStandardEmission]:
+        return self.all()[:limit]
+
+    def read_emission_by_id(self, emission_id: int) -> WRFStandardEmission | None:
+        return self.filter(id=emission_id).first()
+
+    def delete_data_by_id(self, emission_id: int):
+        return self.filter(id=emission_id).delete()
