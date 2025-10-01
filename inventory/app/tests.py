0d0b5e30295921b749aeb2232898eaaafc8b22a3
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-10-01 12:07:43 -0300

    added tests for the subcategory property in emission class

--- inventory/app/tests.py
+++ inventory/app/tests.py
@@ -5,16 +5,35 @@ from .models.cnh_subcategory import CNHSubcategory
 
 
 class WRFEmissionTests(TestCase):
-    def test_create_emission(self):
-        example_subcategory = CNHSubcategory.E
-
-        emission = WRFStandardEmission(
+    def test_subcategory_getter_returns_correct_object(self):
+        emission_with_subcategory_properties = WRFStandardEmission.objects.create(
+            subcategory_name="Lightweight",
+            subcategory_deterioration_factor=1.2,
+            subcategory_category_consumption=10.5,
             fuel="Gasoline",
-            subcategory=example_subcategory,
-            fraction=0.1,
-            mileage=10000,
+            fraction=0.5,
+            mileage=1000,
         )
+        emission_subcategory = emission_with_subcategory_properties.subcategory
 
-        emission.save()
+        self.assertEqual(emission_subcategory.name, "Lightweight")
+        self.assertEqual(emission_subcategory.deterioration_factor, 1.2)
+        self.assertEqual(emission_subcategory.category_consumption, 10.5)
 
-        self.assertEqual(emission.subcategory, example_subcategory.value)
+    def test_subcategory_setter_updates_model_fields(self):
+        emission_to_be_setted = WRFStandardEmission(
+            fuel="Gasoline", fraction=0.5, mileage=1000
+        )
+        emission_to_be_setted.subcategory = CNHSubcategory.A
+
+        self.assertEqual(
+            emission_to_be_setted.subcategory_name, CNHSubcategory.A.value.name
+        )
+        self.assertEqual(
+            emission_to_be_setted.subcategory_deterioration_factor,
+            CNHSubcategory.A.value.deterioration_factor,
+        )
+        self.assertEqual(
+            emission_to_be_setted.subcategory_category_consumption,
+            CNHSubcategory.A.value.category_consumption,
+        )
