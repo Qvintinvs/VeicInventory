bb51301f56333a5172f356d43bcb02b731a7a6b0
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-10-01 02:39:36 -0300

    [Test] added test for the wrf model creation

--- inventory/app/tests.py
+++ inventory/app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import WRFStandardEmission
+from .models.cnh_subcategory import CNHSubcategory
+
+
+class WRFEmissionTests(TestCase):
+    def test_create_emission(self):
+        example_subcategory = CNHSubcategory.E
+
+        emission = WRFStandardEmission(
+            fuel="Gasoline",
+            subcategory=example_subcategory,
+            fraction=0.1,
+            mileage=10000,
+        )
+
+        emission.save()
+
+        self.assertEqual(emission.subcategory, example_subcategory.value)
