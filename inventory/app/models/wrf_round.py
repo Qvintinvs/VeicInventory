99666ed4149c334d5f0de7937e9e7f7d33578588
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-10-01 23:25:17 -0300

    removed WRFRound foreign key for vehicle

--- inventory/app/models/wrf_round.py
+++ inventory/app/models/wrf_round.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 from .wrf_round_status import WRFRoundStatus
-from .wrf_standard_emission import WRFStandardEmission
 
 
 class WRFRound(models.Model):
@@ -12,18 +11,9 @@ class WRFRound(models.Model):
     output_file_path = models.CharField(max_length=255, blank=False)
     namelist = models.TextField(blank=False)
 
-    vehicle = models.ForeignKey(
-        WRFStandardEmission,
-        on_delete=models.CASCADE,
-        related_name="wrf_rounds",
-    )
-
-    def __init__(
-        self, output_file_path: str, namelist: str, vehicle: WRFStandardEmission
-    ):
+    def __init__(self, output_file_path: str, namelist: str):
         self.output_file_path = output_file_path
         self.namelist = namelist
-        self.vehicle = vehicle
 
     def run_if_pending(self):
         if self.status == WRFRoundStatus.PENDING:
