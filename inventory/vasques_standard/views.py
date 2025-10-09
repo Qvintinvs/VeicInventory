0fcefa15fcb759d42c0e2fc9c2af3bd58e20f71d
OQuintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-10-09 10:23:55 -0300

    added form City mock

--- inventory/vasques_standard/views.py
+++ inventory/vasques_standard/views.py
@@ -1,10 +1,13 @@
-from django import forms
+from django import forms, views
 from django.shortcuts import render
 
-from .models import VasquesEmission
+from .models import City, VasquesEmission
 
 
 class VasquesEmissionForm(forms.ModelForm):
     class Meta:
         model = VasquesEmission
         exclude = ("city",)
+
+    def clean_city(self):
+        return City("Itajaí", 1.1)
