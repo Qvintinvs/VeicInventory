64ee10ed51ca3e633a5abf8a48c1837b20fdb885
OQuintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-10-09 11:03:00 -0300

    deleted unused test case

--- inventory/wrf_standard/tests.py
+++ inventory/wrf_standard/tests.py
@@ -1,14 +1,3 @@
 from django.test import TestCase
 
-
-class WRFEmissionTests(TestCase):
-    """
-    WRFStandardEmission examples:
-
-    emission_with_subcategory_properties = WRFStandardEmission.objects.create(
-        subcategory_name=CNHChoices.E,
-        fuel="Gasoline",
-        fraction=0.5,
-        mileage=1000,
-    )
-    """
+# Create your tests here.
