2a038c1259b787244bd2103f979b9267474219ed
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2025-10-11 04:15:47 -0300

    removed netcdf database

--- inventory/rounds/models.py
+++ inventory/rounds/models.py
@@ -43,12 +43,3 @@ class WRFRound(models.Model):
         if self.status == RoundStatus.RUNNING:
             self.status = RoundStatus.COMPLETED
             self.save(update_fields=("status", "timestamp"))
-
-
-class NETCDFBlob(models.Model):
-    wrf_round = models.OneToOneField(
-        WRFRound,
-        on_delete=models.CASCADE,
-        related_name="netcdf_blob",
-    )
-    netcdf_data = models.BinaryField()
