81410908dc0bab4ad9847754fe1630c391bbdc19
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-11-03 03:52:25 -0300

    added storages to apps

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_rq",
+    "storages",
     "rounds",
     "wrf_standard",
     "vasques_standard",
