c6ee5e6d290bdf63f16f0c49089b469f7b2c772e
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-11-04 07:39:57 -0300

    added S3 storage config for the blobs

--- inventory/rounds/storage.py
+++ inventory/rounds/storage.py
@@ -2,8 +2,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class NetCDFMinioStorage(S3Boto3Storage):
+    bucket_name = "wrfchemi-blobs"
     default_acl = "private"
-    file_overwrite = True
+    file_overwrite = False
     custom_domain = False
 
     def read_netcdf(self, name):
