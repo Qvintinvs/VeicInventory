df0f5b7bb25e346abaf84ad7b69b0bbbe0d94740
oQuintino <matheus.quintinodasilva0@gmail.com>
2025-11-05 10:35:46 -0300

    added minio url env variable

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -152,7 +152,7 @@ RQ_QUEUES = {"default": {"USE_REDIS_CACHE": "default"}}
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_S3_ENDPOINT_URL = "http://minio:9000"
+AWS_S3_ENDPOINT_URL = os.getenv("MINIO_URL")
 AWS_ACCESS_KEY_ID = os.getenv("MINIO_ROOT_USER")
 AWS_SECRET_ACCESS_KEY = os.getenv("MINIO_ROOT_PASSWORD")
 AWS_S3_REGION_NAME = "us-east-1"
